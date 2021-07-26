da2df16d96a58e5705adf3b192995cbc9ee6b3eb
Carlos Miguel <carlosmlpimenta@gmail.com>
2021-07-26 18:56:29 +0100

    loading final 1

--- components/loader/Loader.tsx
+++ components/loader/Loader.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 
 import { FlexColCenter } from '@libs/components/flex/Flex';
+import Image from '@libs/components/image/Image';
+import icons from '@images/icons.json';
+import profile from '@images/profile.json';
 import styles from './Loader.module.css';
 
 function Loader() {
@@ -13,9 +16,19 @@ function Loader() {
 	}, []);
 
 	return (
-		<FlexColCenter
-			className={`${styles.block} ${loading ? '' : styles.hide}`}
-		></FlexColCenter>
+		<FlexColCenter className={`${styles.block} ${loading ? '' : styles.hide}`}>
+			<Image
+				src={profile.logo}
+				alt='Carlos Pimenta Logo'
+				className={styles.logo}
+			/>
+			<h1 className={styles.title}>CARLOS PIMENTA</h1>
+			<Image
+				src={icons.loading}
+				alt='Carlos Pimenta Loading'
+				className={styles.loading}
+			/>
+		</FlexColCenter>
 	);
 }
 
