02d841219f565ba72884af9dcd97a47bd76a17ee
Carlos Miguel <carlosmlpimenta@gmail.com>
2021-07-26 21:24:35 +0100

    rip

--- components/landscape/Landscape.tsx
+++ components/landscape/Landscape.tsx
@@ -16,11 +16,9 @@ function Landscape() {
 
 		changeWindowSize();
 
-		window.addEventListener('orientationchange', changeWindowSize);
 		window.addEventListener('resize', changeWindowSize);
 
 		return () => {
-			window.removeEventListener('orientationchange', changeWindowSize);
 			window.removeEventListener('resize', changeWindowSize);
 		};
 	}, []);
